1b4a374628b20f35b01eb85debca375dbfd6e51f
Seba <rodrigo.tolaba@davinci.edu.ar>
2024-12-09 00:55:33 -0300

    Regex y armado DF

--- services/folder_service.py
+++ services/folder_service.py
@@ -5,6 +5,7 @@ from services.file_service import FileProcessor
 from utils.validation_utils import validate_year_month
 from PIL import Image
 import services.func_extrac_data as look_data
+import pandas as pd
 
 # Diccionario para traducir meses entre inglés y español
 months_translator = {
@@ -37,6 +38,7 @@ class FolderProcessor:
         year_folder_name = str(year)
         translated_month = self.translate_month(month)  # Traduce el mes al idioma necesario
         missing_folders = []
+        df_weekly = pd.DataFrame()
 
         for client_folder in os.listdir(parent_folder):
             client_path = os.path.join(parent_folder, client_folder)
@@ -51,7 +53,20 @@ class FolderProcessor:
                 continue
 
             print(f"Procesando carpeta: {os.path.abspath(target_path)}")
-            self.process_weekly_files(target_path, year, translated_month)
+            df_weekly = self.process_weekly_files(target_path, year, translated_month)
+
+        df_filtrado = df_weekly.groupby('fecha_pdf').agg({ 
+            'Name': 'first', 
+            'federal_tax_941': 'max', 
+            'state_tax_edd': 'max', 
+            '941_payment_amount': 'max', 
+            'EDD_payment_amount': 'max' }).reset_index()
+
+        ruta_archivo_fil = r'Data\Output\datos_filtrados.xlsx'
+        ruta_archivo = r'Data\Output\datos.xlsx'
+        
+        df_filtrado.to_excel(ruta_archivo_fil, index=False)
+        df_weekly.to_excel(ruta_archivo, index=False)
 
         if missing_folders:
             self._log_missing_folders(missing_folders, payroll_folder_name, year_folder_name, translated_month)
@@ -85,6 +100,10 @@ class FolderProcessor:
         :param year: Año seleccionado.
         :param month: Mes traducido.
         """
+
+        columnas = ['tipo_archivo', 'fecha_pdf', 'Name', 'federal_tax_941', 'state_tax_edd','941_payment_amount', 'EDD_payment_amount', 'account_number', 'date_pay_settle']
+        df = pd.DataFrame(columns=columnas)
+
         for file_name in os.listdir(folder_path):
             file_path = os.path.join(folder_path, file_name)
 
@@ -94,14 +113,18 @@ class FolderProcessor:
             # Verificar si el archivo es relevante
             if not any(file_name.endswith(suffix + ".pdf") for suffix in ["EDD", "941", f"{year}"]):
                 continue
-
+        
             print(f"Procesando archivo: {os.path.abspath(file_path)}")
 
             # Verificar si el archivo es editable o aplicar OCR
             text = self.process_file_with_ocr(file_path)
 
             # Manejar el texto extraído
-            self.handle_extracted_data(file_name, text, month, year)
+            datos = self.handle_extracted_data(file_name, text, month, year)
+        
+            df = pd.concat([df, datos], ignore_index=True)
+
+        return df
 
     def process_file_with_ocr(self, file_path):
         """
@@ -130,39 +153,62 @@ class FolderProcessor:
         """
         Maneja los datos extraídos del texto según el tipo de archivo.
         """
-        # data_look_for_941 = {
-        #     'payment_amount' : (look_data.extract_payment_amount, 1),
-        #     'account_number' : (look_data.extract_account_number),
-        #     'payer_name' : (look_data.extract_payer_name)
-        # }
 
-        # data_look_for_EED = {
-        #     'payment_amount' : (look_data.extract_payment_amount, 1),
-        #     'account_number' : (look_data.extract_account_number),
-        #     'name' : (look_data.extract_name),
-        #     'payment_date': (look_data.extract_payment_date)
-        # }
+        def format_date(date_str): 
+            """Formatea las fechas al formato YYYY-MM-DD""" 
+            try: 
+                return pd.to_datetime(date_str).strftime('%Y-%m-%d') 
+            except ValueError: 
+                return date_str
 
         type_file = file_name.replace('.pdf','.txt')
-        ruta_prueba = rf"C:\Users\seba\Desktop\Proyectos\Leonardo-Matias\procesamiento_archivos\Data\Output\{type_file}"
+        ruta_prueba = rf"C:\Users\seba\Desktop\Proyectos\Repo_Leo\procesamiento_archivos\Data\Output\{type_file}"
+        data_to_extract = ""
 
         if file_name.endswith("941.pdf"):
+            print(f"*****************************************************")
             print(f"Extrayendo datos de archivo 941: {file_name}")
-            data_to_extract = look_data.extract_payment_amount(text, 1)
-            data_to_extract = look_data.extract_account_number(text)
+            print(f"*****************************************************")
+            data = {
+                'tipo_archivo': "941",
+                'fecha_pdf': file_name.replace('941.pdf', ''), 
+                'Name': look_data.extract_payer_name(text), 
+                '941_payment_amount': look_data.extract_payment_amount_941(text), 
+                'EDD_payment_amount': "0",
+                'account_number': look_data.extract_account_number(text), 
+                'date_pay_settle': format_date(look_data.extract_settlement_date(text))
+            }
 
         elif file_name.endswith("EDD.pdf"):
+            print(f"*****************************************************")
             print(f"Extrayendo datos de archivo EDD: {file_name}")
-            data_to_extract = ""
+            print(f"*****************************************************")
+            data = {
+                'tipo_archivo': "EDD",
+                'fecha_pdf': file_name.replace('EDD.pdf', ''), 
+                'Name': look_data.extract_name(text), 
+                '941_payment_amount': "0", 
+                'EDD_payment_amount': look_data.extract_payment_amount_edd(text, 2),
+                'account_number': look_data.extract_account_number(text), 
+                'date_pay_settle': format_date(look_data.extract_payment_date(text))
+            }
 
         else:
+            print(f"*****************************************************")
             print(f"Extrayendo datos de archivo de fecha simple: {file_name}")
+            print(f"*****************************************************")
             # Lógica específica para manejar archivos de fechas 
-            data_to_extract = text
+            data = {
+                'tipo_archivo': "general",
+                'fecha_pdf': file_name.replace('.pdf',''), 
+                'Name': look_data.extract_company_name(text), 
+                'federal_tax_941': look_data.extract_payment_amount_general_941(text),
+                'state_tax_edd': look_data.extract_payment_amount_general_edd(text)
+            }
+        # with open(ruta_prueba, 'w', encoding='utf-8') as archivo_txt:
+        #         archivo_txt.write(text)
 
-        
-        with open(ruta_prueba, 'w', encoding='utf-8') as archivo_txt:
-                archivo_txt.write(data_to_extract)
+        return pd.DataFrame([data])
 
     def translate_month(self, month):
         """Traduce un mes entre inglés y español."""
--- services/ocr_service.py
+++ services/ocr_service.py
@@ -18,3 +18,43 @@ class OCRHandler:
         for image in images:
             extracted_text += pytesseract.image_to_string(image)
         print(f"Texto extraído:\n{extracted_text}")
+
+
+
+    # document = fitz.open(pdfpath)
+    # account_number = None
+
+    # for page_num in range(len(document)):
+    #     # Extraer la página como imagen
+    #     page = document.load_page(page_num)
+    #     pix = page.get_pixmap(dpi=300)  # Resolución alta para mejor OCR
+    #     img = Image.open(io.BytesIO(pix.tobytes("png")))
+
+    #     # Preprocesar la imagen
+    #     img_cv = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2GRAY)  # Escala de grises
+    #     img_cv = cv2.fastNlMeansDenoising(img_cv, None, 30, 7, 21)  # Reducción de ruido
+    #     , img_cv = cv2.threshold(img_cv, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)  # Binarización
+
+    #     # Convertir de nuevo a PIL
+    #     processed_img = Image.fromarray(img_cv)
+
+    #     # Realizar OCR
+    #     ocr_text = pytesseract.image_to_string(processed_img, lang=lang, config='--psm 6')
+
+    #     print(f"Texto extraído de la página {page_num + 1}:\n{ocr_text}")  # Debugging: Ver texto extraído
+
+    #     # Buscar la línea que contiene "Account Number"
+    #     for line in ocr_text.split('\n'):
+    #         if "Account Number" in line:
+    #             # Extraer el texto asociado
+    #             match = re.search(r'Account Number[:\s]*([^\n]+)', line, re.IGNORECASE)
+    #             if match:
+    #                 raw_number = match.group(1).strip()  # Capturar el valor después de "Account Number"
+
+    #                 # Limpiar y reconstruir el formato "XXXX"
+    #                 digits = ''.join(filter(str.isdigit, raw_number))  # Extraer solo dígitos
+    #                 if len(digits) >= 4:
+    #                     account_number = f"{digits[-4:]}"  # Usar los últimos 4 dígitos
+    #                 else:
+    #                     account_number = raw_number  # Dejar el texto crudo si no es válido
+    #                 break
